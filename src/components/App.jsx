d529b1234706b2bc6852c9a519e590088ed1237b
Vladislav Zmaga <vladislavzmaga@gmail.com>
2022-11-13 00:23:04 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,7 +23,9 @@ export const App = () => {
             <Route path="cast" element={<Cast />}></Route>
             <Route path="reviews" element={<Reviews />}></Route>
           </Route>
+          <Route path="*" element={<MoviesList />}></Route>
         </Route>
+        <Route path="*" element={<Layout />}></Route>
       </Routes>
     </div>
   );
